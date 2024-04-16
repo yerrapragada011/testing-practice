9e6a2216e1c23f3a52dffa7f7718c2f5dad73695
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-04-16 15:20:57 -0400

    Add capitalize function & test

---
+++ script.js
@@ -0,0 +1,5 @@
+export function capitalize(string) {
+  let firstLetter = string.charAt(0)
+  let firstLetterCap = firstLetter.toUpperCase()
+  return firstLetterCap
+}
---
+++ script.test.js
@@ -0,0 +1,5 @@
+import { capitalize } from './script.js'
+
+test('capitalize', () => {
+    expect(capitalize('b')).toBe('B')
+})
