e1eab597b3b7e3d3f564f08a17088e7cc5be8b62
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-04-16 15:39:27 -0400

    Add calculator object with basic operations & tests

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ export function capitalize(string) {
 export function reverseString(string) {
   return string.split('').reverse().join('')
 }
+
+class Calculator {
+  add(num1, num2) {
+    return num1 + num2
+  }
+
+  subtract(num1, num2) {
+    return num1 - num2
+  }
+
+  multiply(num1, num2) {
+    return num1 * num2
+  }
+
+  divide(num1, num2) {
+    return num1 / num2
+  }
+}
+
+export const calculator = new Calculator()
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from './script.js'
+import { capitalize, reverseString, calculator } from './script.js'
 
 test('Return the first letter of a string capitalized', () => {
   expect(capitalize('test')).toBe('T')
@@ -6,4 +6,11 @@ test('Return the first letter of a string capitalized', () => {
 
 test('Return a string reversed', () => {
   expect(reverseString('test')).toBe('tset')
+})
+
+test('Return the calulation of two numbers', () => {
+  expect(calculator.add(1, 2)).toBe(3)
+  expect(calculator.subtract(1, 2)).toBe(-1)
+  expect(calculator.multiply(1, 2)).toBe(2)
+  expect(calculator.divide(1, 2)).toBe(0.5)
 })
