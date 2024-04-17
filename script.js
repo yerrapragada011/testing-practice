bf1736b343703c1b617cd2db1e258d31c27d80af
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-04-16 23:14:54 -0400

    Add caesarCipher func & tests

--- script.js
+++ script.js
@@ -27,3 +27,53 @@ class Calculator {
 }
 
 export const calculator = new Calculator()
+
+export function caesarCipher(string, shift) {
+  let alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ]
+
+  let result = ''
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i]
+    if (alphabet.includes(char)) {
+      let index = alphabet.indexOf(char)
+      let newIndex = index + shift
+
+      if (newIndex < 0) {
+        newIndex = alphabet.length + newIndex
+      }
+
+      newIndex = newIndex % alphabet.length
+
+      result += alphabet[newIndex]
+    }
+  }
+  return result
+}
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString, calculator } from './script.js'
+import { capitalize, reverseString, calculator, caesarCipher } from './script.js'
 
 test('Return the first letter of a string capitalized', () => {
   expect(capitalize('test')).toBe('T')
@@ -13,4 +13,9 @@ test('Return the calulation of two numbers', () => {
   expect(calculator.subtract(1, 2)).toBe(-1)
   expect(calculator.multiply(1, 2)).toBe(2)
   expect(calculator.divide(1, 2)).toBe(0.5)
+})
+
+test('Return each character of a string shifted by a value', () => {
+    expect(caesarCipher('test', 3)).toBe('whvw')
+    expect(caesarCipher('zack', 3)).toBe('cdfn')
 })
